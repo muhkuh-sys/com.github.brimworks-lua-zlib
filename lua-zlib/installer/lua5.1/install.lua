0b97e89f08fd134bd51e8369c890b179c44077a6
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-02-12 17:52:21 +0100

    Build the packages.

---
+++ lua-zlib/installer/lua5.1/install.lua
@@ -0,0 +1,42 @@
+local t = ...
+local strDistId, strDistVersion, strCpuArch = t:get_platform()
+
+if strDistId=='windows' then
+  if strCpuArch=='x86' then
+    t:install('windows_x86/lua_plugins/zlib.dll',            '${install_lua_cpath}/')
+  elseif strCpuArch=='x86_64' then
+    t:install('windows_x86_64/lua_plugins/zlib.dll',         '${install_lua_cpath}/')
+  end
+
+elseif strDistId=='ubuntu' then
+  if strDistVersion=='12.04' then
+    if strCpuArch=='x86' then
+      t:install('ubuntu_1204_x86/lua_plugins/zlib.so',       '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1204_x86_64/lua_plugins/zlib.so',    '${install_lua_cpath}/')
+    end
+
+  if strDistVersion=='14.04' then
+    if strCpuArch=='x86' then
+      t:install('ubuntu_1404_x86/lua_plugins/zlib.so',       '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1404_x86_64/lua_plugins/zlib.so',    '${install_lua_cpath}/')
+    end
+
+    if strDistVersion=='16.04' then
+      if strCpuArch=='x86' then
+      t:install('ubuntu_1604_x86/lua_plugins/zlib.so',       '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1604_x86_64/lua_plugins/zlib.so',    '${install_lua_cpath}/')
+    end
+
+    if strDistVersion=='16.10' then
+      if strCpuArch=='x86' then
+      t:install('ubuntu_1610_x86/lua_plugins/zlib.so',       '${install_lua_cpath}/')
+    elseif strCpuArch=='x86_64' then
+      t:install('ubuntu_1610_x86_64/lua_plugins/zlib.so',    '${install_lua_cpath}/')
+    end
+  end
+end
+
+return true
